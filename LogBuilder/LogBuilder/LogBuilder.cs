7a0a40644b76cb59619567d1eb7d46723b7e0999
TakinosaJi <kostiantyn.chomakov@gmail.com>
2021-08-25 17:07:43 +0300

    Add LogBuilder itself and LoggerExtensions

---
+++ LogBuilder/LogBuilder/LogBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogBuilder
+{
+    public class LogBuilder : IEnumerable<KeyValuePair<string, object>>
+    {    
+        private readonly List<KeyValuePair<string, object>> _logProperties = new();
+        
+        private string _message { get; }
+        
+        public LogBuilder(string message)
+        {
+            _message = message;
+        }
+        
+        public LogBuilder(string message, params (string name, object value)[] logProperties)
+        {
+            _message = message;
+            AppendProperties(logProperties);
+        }
+        
+        public static Func<LogBuilder, Exception, string> Formatter { get; } =
+            (l, e) => l._message;
+        
+        public LogBuilder WithProperties(params (string name, object value)[] logArgs)
+        {
+            AppendProperties(logArgs);
+            return this;
+        }
+        
+        private void AppendProperties(params (string name, object value)[] logArgs) =>
+            _logProperties.AddRange(logArgs
+                .Select(item => new KeyValuePair<string, object>(
+                    item.name,
+                    item.value)));
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() =>
+            _logProperties.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LogBuilder/LogBuilder/LoggerExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace LogBuilder
+{
+    public static class LoggerExtensions
+    {
+        public static void LogInformationWith(
+            this ILogger logger,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Information, message, null, properties);
+
+        public static void LogDebugWith(
+            this ILogger logger,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Debug, message, null, properties);
+
+        public static void LogTraceWith(
+            this ILogger logger,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Trace, message, null, properties);
+
+        public static void LogErrorWith(
+            this ILogger logger,
+            Exception exception,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Error, message, exception, properties);
+
+        public static void LogErrorWith(
+            this ILogger logger,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Error, message, null, properties);
+
+        public static void LogWarningWith(
+            this ILogger logger,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, LogLevel.Warning, message, null, properties);
+
+        public static void LogWithProperties(
+            this ILogger logger,
+            LogLevel level,
+            string message,
+            params (string key, object value)[] properties) =>
+            LogWithProperties(logger, level, message, null, properties);
+
+        public static void LogWithProperties(
+            this ILogger logger,
+            LogLevel level,
+            string message,
+            Exception? exception,
+            params (string key, object value)[] properties)
+        {
+            logger.LogWithBuilder(level, exception, new LogBuilder(message, properties));
+        }
+
+        public static void LogWithBuilder(
+            this ILogger logger,
+            LogLevel level,
+            Exception? exception,
+            LogBuilder logBuilder) =>
+            logger.Log(level, default, logBuilder, exception, LogBuilder.Formatter);
+    }
+}
